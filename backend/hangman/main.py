5d3dc16c8002d6fdf992118c7abf9c4286c8b66e
Drew <tullos.andrew@gmail.com>
2024-07-14 19:04:08 -0500

    python updated - flask set up

--- backend/hangman/main.py
+++ backend/hangman/main.py
@@ -1,15 +1,17 @@
-from flask import Flask, jsonify
 import random
-
-app = Flask(__name__)
 
 word_list = ["greenish", "abruptly", "subway"]
 
 @app.route('/get-char-count', methods=['GET'])
-def get_char_count():
-      word_picked = random.choice(word_list)
-      char_count = len(word_picked)
-      return jsonify({"char_count": char_count})
+def get_char():
+   word_picked = random.choice(word_list)
+   char_count = len(word_picked)
+   return jsonify({"char_count": char_count})
+
+
+
+print(word_picked, char_count)
 
 if __name__ == '__main__':
       app.run(debug=True)
+
