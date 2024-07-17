5aad6d3fa6abf3eb75db0a4a0c5fb36686915964
Drew <tullos.andrew@gmail.com>
2024-07-16 20:50:33 -0500

    Keyboard

--- backend/hangman/main.py
+++ backend/hangman/main.py
@@ -7,16 +7,22 @@ CORS(app)
 
 # Rework this to ChatGPT call to select word
 word_list = ["ethical", "roast", "subway"]
+word_picked = ""
 
 # Get route - sending json data for svg
 @app.route('/get-char-count', methods=['GET'])
 def get_char():
-   word_picked = random.choice(word_list)
+   global word_picked  
+   word_picked = random.choice(word_list)  
    char_count = len(word_picked)
    print(word_picked, char_count)
    return jsonify({"char_count": char_count})
 
 
+# @app.route('/get-check-letter', methods=['GET'])
+# def check_letter():
+#    for letter in word_picked:
+#       if letter == 
 
 if __name__ == '__main__':
    app.run(debug=True)
--- frontend/src/app/components/Keyboard.jsx
+++ frontend/src/app/components/Keyboard.jsx
@@ -1,43 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+// const [input, setLetter] = useState("");
+
+function HandleKeyStroke() {
+	alert("Yo");
+}
 
 function Keyboard() {
 	return (
 		<div className="group rounded-lg border-4 border-neutral-700 px-5 py-4 transition-colors hover:border-gray-300 bg-neutral-800/30 text-turquoise">
 			<div className="my-1 flex w-full justify-center gap-1">
-				<kbd className="kbd">q</kbd>
-				<kbd className="kbd">w</kbd>
-				<kbd className="kbd">e</kbd>
-				<kbd className="kbd">r</kbd>
-				<kbd className="kbd">t</kbd>
-				<kbd className="kbd">y</kbd>
-				<kbd className="kbd">u</kbd>
-				<kbd className="kbd">i</kbd>
-				<kbd className="kbd">o</kbd>
-				<kbd className="kbd">p</kbd>
+				{["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"].map((key) => (
+					<kbd key={key} className="kbd">
+						{key}
+					</kbd>
+				))}
 			</div>
 			<div className="my-1 flex w-full justify-center gap-1">
-				<kbd className="kbd">a</kbd>
-				<kbd className="kbd">s</kbd>
-				<kbd className="kbd">d</kbd>
-				<kbd className="kbd">f</kbd>
-				<kbd className="kbd">g</kbd>
-				<kbd className="kbd">h</kbd>
-				<kbd className="kbd">j</kbd>
-				<kbd className="kbd">k</kbd>
-				<kbd className="kbd">l</kbd>
+				{["a", "s", "d", "f", "g", "h", "j", "k", "l"].map((key) => (
+					<kbd key={key} className="kbd">
+						{key}
+					</kbd>
+				))}
 			</div>
 			<div className="my-1 flex w-full justify-center gap-1">
-				<kbd className="kbd">z</kbd>
-				<kbd className="kbd">x</kbd>
-				<kbd className="kbd">c</kbd>
-				<kbd className="kbd">v</kbd>
-				<kbd className="kbd">b</kbd>
-				<kbd className="kbd">n</kbd>
-				<kbd className="kbd">m</kbd>
-				<kbd className="kbd">/</kbd>
+				{["z", "x", "c", "v", "b", "n", "m", "/"].map((key) => (
+					<kbd key={key} className="kbd">
+						{key}
+					</kbd>
+				))}
 			</div>
 			<div className="my-1 flex w-full justify-center gap-1">
-				<button className=" kbd btn btn-wide text-turquoise">Submit</button>
+				<button
+					className="kbd btn btn-wide text-turquoise"
+					onClick={HandleKeyStroke}
+				>
+					Submit
+				</button>
 			</div>
 		</div>
 	);
